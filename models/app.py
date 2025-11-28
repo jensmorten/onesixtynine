ee9353df78eb158dc15bd9f7650980dc9ad0a927
jensmorten <jens.morten.nilsen@gmail.com>
2025-11-28 14:50:10 +0100

    Update app.py

--- models/app.py
+++ models/app.py
@@ -178,7 +178,8 @@ def hybrid_var_ml_forecast(df, n_months, var_lags, lags_ML, tau, vol_window, min
             regime_weight = (rs - 0.8) / (1.2 - 0.8)  # linear ramp [0,1]
         else:
             regime_weight = 1.0         # regime change
-
+        
+        regime_weight = 1.0 
         # --- forecasting ---
         win = df.values[-lags_ML:].copy()
 
